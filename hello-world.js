311688d9e6bd3c77d4298da33ca0d389271078b3
Tianyu Li <18642966+ty-cs@users.noreply.github.com>
2021-07-21 17:14:15 +0800

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,9 @@
 
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    new Response("Hello world", {
+      status: 200,
+      headers: { "content-type": "text/plain" },
+    }),
+  );
+});
