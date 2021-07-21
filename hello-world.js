b5f4e62992359a871ceba91596ca0d1a1b30b6b3
Tianyu Li <18642966+ty-cs@users.noreply.github.com>
2021-07-21 09:54:24 +0000

    feat: return ip

--- hello-world.js
+++ hello-world.js
@@ -1,7 +1,11 @@
 
 addEventListener("fetch", (event) => {
+  // Get the value of the header named `x-forwarded-for`.
+  // The value can either be a IPv4 or IPv6 address.
+  const ip = event.request.headers.get("x-forwarded-for");
+
   event.respondWith(
-    new Response("Hello world", {
+    new Response(`Hello, ${ip}`, {
       status: 200,
       headers: { "content-type": "text/plain" },
     }),
