32f4504b727534fffe8de046eda4decc2f6ff49d
Tianyu Li <18642966+ty-cs@users.noreply.github.com>
2021-08-21 22:43:01 +0000

    feat: return json

--- hello-world.ts
+++ hello-world.ts
@@ -3,11 +3,15 @@ addEventListener("fetch", (event) => {
   // Get the value of the header named `x-forwarded-for`.
   // The value can either be a IPv4 or IPv6 address.
   const ip = event.request.headers.get("x-forwarded-for");
+
   const json = JSON.stringify({ip});
+
   event.respondWith(
     new Response(json, {
       status: 200,
-      headers: { "content-type": "text/plain" },
+      headers: {
+        "content-type": "application/json; charset=UTF-8",
+      },
     }),
   );
 });
