8f91cc5a664ed9d6ce2546c1dd87c6b34d7e7b4c
Tianyu Li <18642966+ty-cs@users.noreply.github.com>
2021-08-22 06:38:57 +0800

    Update hello-world.ts

--- hello-world.ts
+++ hello-world.ts
@@ -3,9 +3,9 @@ addEventListener("fetch", (event) => {
   // Get the value of the header named `x-forwarded-for`.
   // The value can either be a IPv4 or IPv6 address.
   const ip = event.request.headers.get("x-forwarded-for");
-
+  const json = JSON.stringify({ip});
   event.respondWith(
-    new Response({ip}, {
+    new Response(json, {
       status: 200,
       headers: { "content-type": "text/plain" },
     }),
