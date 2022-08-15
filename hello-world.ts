1cafd316a349fe00e5e43e07f5e72b2026b33344
Tianyu Li <18642966+ty-cs@users.noreply.github.com>
2022-08-16 00:14:26 +0800

    Update hello-world.ts

--- hello-world.ts
+++ hello-world.ts
@@ -1,17 +1,16 @@
+import { ConnInfo, serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
-addEventListener("fetch", (event) => {
-  // Get the value of the header named `x-forwarded-for`.
-  // The value can either be a IPv4 or IPv6 address.
+function handler(_req: Request, connInfo: ConnInfo) {
   const addr = connInfo.remoteAddr as Deno.NetAddr;
   const ip = addr.hostname;
   const json = JSON.stringify({ip});
 
-  event.respondWith(
-    new Response(json, {
+  return new Response(json, {
       status: 200,
       headers: {
         "content-type": "application/json; charset=UTF-8",
       },
-    }),
-  );
-});
+  });
+}
+
+serve(handler);
